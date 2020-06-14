beed5b09cffa343c72796e669af2d7d044964e03
kinkydesu <dzakharovv@gmail.com>
2020-06-14 12:51:57 +0300

    Первый коммит

---
+++ script/script.js
@@ -0,0 +1,20 @@
+let money = 210000;
+let income = 'Upwork';
+let addExpenses = 'Wife, Children, School, Travel';
+let deposit = true;
+let mission = 999999999;
+let period = 10;
+
+
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен', period, 'месяцев');
+console.log("Цель заработать", mission, "рублей");
+console.log(addExpenses.toLowerCase().split(' '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
+
